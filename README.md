5d959cef0ed8cff25f06c74587a51db16442171f
El Mehdi AAYADI <elmehdiaayadi@gmail.com>
2019-09-29 14:17:48 +0200

    Finalisation du tp calculette

---
+++ README.md
@@ -0,0 +1,9 @@
+/// tp de la calculette
+
+fait le meme traitement qu'une calculette scientifique
+- n'accepte pas d'opération s'il n'y a pas de second numero
+- on peut faire des opération sur l'ancien valeur calculé
+- historique est un scroll view pour afficher tous les resultats
+- le switch entre AC(all clear) et C(clear) , pour refaire la derniere saisie et pour refaire tout le calcul
+
+
--- tpcalculette/src/main/java/com/midotechno/tpcalculette/MainActivity.kt
+++ tpcalculette/src/main/java/com/midotechno/tpcalculette/MainActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.text.method.ScrollingMovementMethod
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,39 +20,41 @@ class MainActivity : AppCompatActivity() {
     lateinit var saisieText:TextView
     lateinit var btnReset:Button
 
+
+    // le scroll view affiche les historique avec un scroll
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        historiqueText = findViewById<EditText>(R.id.textView3)
-
-        // cette methode ne permet pas de rendre le textview scrollable ??
-        historiqueText.movementMethod = ScrollingMovementMethod()
-
-
-        saisieText = findViewById<EditText>(R.id.editText2)
-        btnReset = findViewById<Button>(R.id.button16)
-        saisieText.text = "0"
+        historiqueText = findViewById<EditText>(R.id.textViewHist2)
+        saisieText = findViewById<EditText>(R.id.editTextSaisie2)
+        btnReset = findViewById<Button>(R.id.buttonac)
+        saisieText.text = ""
 
     }
 
 
     fun onClick(v:View){
         val element = v as? Button ?: return
-        if (element.text.equals("0") or element.text.equals("1")
+        if ((element.text.equals("0") or element.text.equals("1")
             or element.text.equals("2") or element.text.equals("6")
             or element.text.equals("3") or element.text.equals("7")
             or element.text.equals("4") or element.text.equals("8")
-            or element.text.equals("5") or element.text.equals("9")){
+            or element.text.equals("5") or element.text.equals("9")) or (element.text.equals(".") and !(saisieText.text.contains(".")))){
+
+            // verification aussi sur les virgules
+
             saisieText.text = (saisieText.text.toString() + element.text.toString())
-            btnReset.text = "C"   // au traiment changement du text btn reset ( afin de faire la modif du champs saisie seulement )
+            btnReset.text = "C"   // le changement de ac vers c afin de modifier just l'operation en cours comme en calculatrice scientifique
         }
     }
 
+    // fonction sur les opérations pour définir quel opération on a choisie
     fun onClickOperation(v:View){
         val element = v as? Button ?: return
 
-        if (encours.equals(false)) {
+        if (encours.equals(false) and  !(saisieText.text.equals("")) ) {
             var result = element.text.toString()
             when (result) {
                 "+" -> operation = Op_enum.addition
@@ -63,7 +64,7 @@ class MainActivity : AppCompatActivity() {
             }
             encours=true
             num1 = saisieText.text.toString().toFloat()
-            saisieText.text = ("0".toString())
+            saisieText.text = ("".toString())
         }
 
     }
@@ -77,24 +78,29 @@ class MainActivity : AppCompatActivity() {
 
         val element = v as? Button ?: return
         if (encours.equals(true) and element.text.equals("=")){
-            if (operation.equals(Op_enum.division) and saisieText.text.equals("0") ){
+            if (saisieText.text.equals("")){
 
-                // saisietext.text.tostring.tofloat = 0 ?? c'est la verification qu'il faut faire au lieu de 0
+                // on choisissant une opération et click directe sur result sans saisir un nouveau element
+                // return error
 
-                // afficher infinity au lieu d'error
-                saisieText.text = ("Error")
+                saisieText.text = "Error"
+
             }else{
                 num2 = saisieText.text.toString().toFloat()
                 val op = Operation(operation,num1,num2)
-                saisieText.text = ""
+                saisieText.text = op.result().toString()
                 result = op.result()
                 historiqueText.text = historiqueText.text.toString() +"\n->$num1"+operationSymbole(operation)+"$num2=$result"
-                operation=Op_enum.nothing
-                encours = false
+
             }
+            btnReset.text="AC"
+            operation=Op_enum.nothing
+            encours = false
+
         }
     }
 
+    // je utilise cette fonction pour l'affichage en historique
     fun operationSymbole(op:Op_enum):String{
         when(op) {
             Op_enum.addition -> return "+"
@@ -105,6 +111,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    // le switch entre ac et c clear et clearall
     fun onClickClear(v:View){
         // traitement du modification ou reset global
         // historique reste tjr visible
@@ -112,13 +120,13 @@ class MainActivity : AppCompatActivity() {
         // le cas de modification
         if (element.text.equals("AC")){
             encours = false
-            saisieText.text="0"
+            saisieText.text=""
             num1 = 0f
             num2= 0f
             result=0f
             operation=Op_enum.nothing
         }else{
-            saisieText.text="0"
+            saisieText.text=""
             num2= 0f
             result=0f
             btnReset.text="AC"
